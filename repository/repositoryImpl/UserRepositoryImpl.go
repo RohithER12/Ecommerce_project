46b76790ca71f304596fe8a0b4bdc57cbb58856a
RohithER12 <rohither12@outlook.com>
2023-06-28 19:33:06 +0530

    adiminRepoImpl.go all gorm query changed to row query

--- repository/repositoryImpl/UserRepositoryImpl.go
+++ repository/repositoryImpl/UserRepositoryImpl.go
@@ -88,6 +88,9 @@ func (repo *UserRepositoryImpl) GetByEmail(email string) (*entity.User, error) {
 		return nil, err
 	}
 	defer rows.Close()
+	if !rows.Next() {
+		return nil, errors.New("email not found")
+	}
 	for rows.Next() {
 		var firstName, lastName, email, phoneNumber, username, password string
 		var block, verified bool
@@ -118,6 +121,9 @@ func (repo *UserRepositoryImpl) GetUserByUserName(username string) (*entity.User
 		return nil, err
 	}
 	defer rows.Close()
+	if !rows.Next() {
+		return nil, errors.New("email not found")
+	}
 	for rows.Next() {
 		var firstName, lastName, email, phoneNumber, username, password string
 		var block, verified bool
@@ -147,6 +153,9 @@ func (repo *UserRepositoryImpl) GetByPhoneNumber(phoneNumber string) (*entity.Us
 	}
 
 	defer rows.Close()
+	if !rows.Next() {
+		return nil, errors.New("email not found")
+	}
 	for rows.Next() {
 		var firstName, lastName, email, phoneNumber, username, password string
 		var block, verified bool
